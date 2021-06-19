8088617e04560b85a7126bed013e5404c0091a0d
HaiChienB <84946654+HaiChienB@users.noreply.github.com>
2021-06-19 11:59:04 +0700

    first commit

---
+++ CTQuanLyTiecCuoi/DichVu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CTQuanLyTiecCuoi
+{
+    public class DichVu
+    {
+        public int IdDichVu { get; set; }
+        public string TenDichVu { get; set; }
+        public int DonGia { get; set; }
+    }
+}
